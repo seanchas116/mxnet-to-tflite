c5e0d659b9a4bdb510af47f8449d8903ac5e545f
Ryohei Ikegami <iofg2100@gmail.com>
2020-01-29 22:36:49 +0900

    Save tflite

--- mxnet2tflite.py
+++ mxnet2tflite.py
@@ -3,17 +3,21 @@ import numpy as np
 from mxnet.contrib import onnx as onnx_mxnet
 import onnx
 import onnx_tf
+import tensorflow as tf
 
 parser = ArgumentParser(description='Convert MXNet models to TensorFlow Lite models')
 parser.add_argument('--symbol')
 parser.add_argument('--params')
 parser.add_argument('--input_shape')
+parser.add_argument('--input_name')
+parser.add_argument('--output_name')
 
 args = parser.parse_args()
 
 symbol_path = args.symbol
 params_path = args.params
 input_shape = list(map(int, args.input_shape.split(",")))
+
 
 onnx_path = '__model.onnx'
 
@@ -22,5 +26,16 @@ converted_model_path = onnx_mxnet.export_model(symbol_path, params_path, [input_
 tf_path = '__model.pb'
 
 onnx_model = onnx.load(onnx_path)
+# Maybe you have to do this workaround https://github.com/onnx/onnx-tensorflow/issues/377#issuecomment-464714597
 tf_rep = onnx_tf.backend.prepare(onnx_model)
 tf_rep.export_graph(tf_path)
+
+converter = tf.lite.TFLiteConverter.from_frozen_graph(
+    tf_path,
+    [args.input_name],
+    [args.output_name] 
+)
+tflite_model = converter.convert()
+
+tflite_path = '__model.tflite'
+open(tflite_path, "wb").write(tflite_model)
