3ad54cac33066470cc025be66342a4f84b59aaa2
Ryohei Ikegami <iofg2100@gmail.com>
2020-01-29 22:18:42 +0900

    WIP convert to tensorflow

--- mxnet2tflite.py
+++ mxnet2tflite.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 import numpy as np
 from mxnet.contrib import onnx as onnx_mxnet
+import onnx
+import onnx_tf
 
 parser = ArgumentParser(description='Convert MXNet models to TensorFlow Lite models')
 parser.add_argument('--symbol')
@@ -13,6 +15,12 @@ symbol_path = args.symbol
 params_path = args.params
 input_shape = list(map(int, args.input_shape.split(",")))
 
-onnx_file = '__model.onnx'
+onnx_path = '__model.onnx'
 
-converted_model_path = onnx_mxnet.export_model(symbol_path, params_path, [input_shape], np.float32, onnx_file)
+converted_model_path = onnx_mxnet.export_model(symbol_path, params_path, [input_shape], np.float32, onnx_path)
+
+tf_path = '__model.pb'
+
+onnx_model = onnx.load(onnx_path)
+tf_rep = onnx_tf.backend.prepare(onnx_model)
+tf_rep.export_graph(tf_path)
