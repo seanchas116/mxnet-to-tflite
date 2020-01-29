afdde1aec5905ac96391286f6316b74b292570f1
Ryohei Ikegami <iofg2100@gmail.com>
2020-01-29 22:00:04 +0900

    Get args

--- mxnet2tflite.py
+++ mxnet2tflite.py
@@ -3,6 +3,9 @@ from argparse import ArgumentParser
 parser = ArgumentParser(description='Convert MXNet models to TensorFlow Lite models')
 parser.add_argument('--symbol')
 parser.add_argument('--params')
-parser.add_argument('--inputShape')
+parser.add_argument('--input_shape')
 
 args = parser.parse_args()
+print(args.symbol)
+print(args.params)
+print(args.input_shape)
